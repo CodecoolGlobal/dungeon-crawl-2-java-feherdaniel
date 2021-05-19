185290b0daebe6da13b13f0b4feee74599c753a6
samrcell91 <smarcell91@gmail.com>
2021-05-19 09:44:37 +0200

    Game State Dao Jdbc methods

--- src/main/java/com/codecool/dungeoncrawl/dao/GameStateDaoJdbc.java
+++ src/main/java/com/codecool/dungeoncrawl/dao/GameStateDaoJdbc.java
@@ -9,6 +9,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GameStateDaoJdbc implements GameStateDao {
+    private DataSource dataSource;
+    private PlayerDaoJdbc pdj;
+
+    GameStateDaoJdbc(DataSource dataSource) {
+        this.dataSource = dataSource;
+        this.pdj = new PlayerDaoJdbc(dataSource);
+    }
+
     @Override
     public void add(GameState state) {
         try (Connection conn = dataSource.getConnection()) {
@@ -29,16 +37,51 @@ public class GameStateDaoJdbc implements GameStateDao {
 
     @Override
     public void update(GameState state) {
-
+        try (Connection conn = dataSource.getConnection()) {
+            String sql = "UPDATE game_state SET current_map = ?, saved_at = ?, player_id = ? WHERE id = ?";
+            PreparedStatement statement = conn.prepareStatement(sql);
+            statement.setString(1, state.getCurrentMap());
+            statement.setDate(2, state.getSavedAt());
+            statement.setInt(3, state.getPlayer().getId());
+            statement.setInt(4, state.getId());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public GameState get(int id) {
-        return null;
+        try (Connection c = dataSource.getConnection()) {
+            String sql = "SELECT current_map, saved_at, player_id FROM game_state WHERE id = ?";
+            PreparedStatement ps = c.prepareStatement(sql);
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+            if (!rs.next()) {
+                return null;
+            }
+            GameState gameState = new GameState(rs.getString(1), rs.getDate(2), pdj.get(rs.getInt(3)));
+            gameState.setId(id);
+            return gameState;
+        } catch (SQLException e) {
+            throw new RuntimeException("Error while getting game state with id: " + id + e);
+        }
     }
 
     @Override
     public List<GameState> getAll() {
-        return null;
+        try (Connection c = dataSource.getConnection()) {
+            String sql = "SELECT id, current_map, saved_at, player_id FROM game_state";
+            ResultSet rs = c.createStatement().executeQuery(sql);
+            List<GameState> result = new ArrayList<>();
+            while (rs.next()) {
+                GameState gameState = new GameState(rs.getString(2), rs.getDate(3), pdj.get(rs.getInt(4)));
+                gameState.setId(rs.getInt(1));
+                result.add(gameState);
+            }
+            return result;
+        } catch (SQLException e) {
+            throw new RuntimeException("Error while getting all game states", e);
+        }
     }
 }
