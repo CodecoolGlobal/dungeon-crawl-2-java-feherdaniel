7a9d3f6539982a5a0e4fa172f9fedd96a7911e28
Daniel Tokai <tokaidaniel@gmail.com>
2021-05-20 16:26:12 +0200

    MapSaver test

--- src/test/java/com/codecool/dungeoncrawl/logic/MapLoaderTest.java
+++ src/test/java/com/codecool/dungeoncrawl/logic/MapLoaderTest.java
@@ -7,9 +7,30 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class MapLoaderTest {
+    private final String testMap =  "#############             \n" +
+                                    "#...........f    ######## \n" +
+                                    "#...s...s...######..g...# \n" +
+                                    "#.....k.....b..d........# \n" +
+                                    "#...........######...i..# \n" +
+                                    "#...........#    #......# \n" +
+                                    "#...........#    #...e..# \n" +
+                                    "#..........g#    #......# \n" +
+                                    "#####..######    ###..### \n" +
+                                    "    #..#           #..#   \n" +
+                                    "    #..#           #..#   \n" +
+                                    "    #..#         ###..### \n" +
+                                    "  ###..####      #......# \n" +
+                                    "  #.......#      #.i.p..# \n" +
+                                    "  #.......#      #......# \n" +
+                                    "  #.......#      #......# \n" +
+                                    "  #.......#      #...c..# \n" +
+                                    "  #.......#      #......# \n" +
+                                    "  #...@...#      #......x \n" +
+                                    "  #########      ######## \n";
+
     @Test
      void loadMap_mapTxtFile_gameMap() {
-        MapLoader.setPlayerName("Tokci");
+        MapLoader.setPlayerName("");
         GameMap map = MapLoader.loadMap("/mapTest.txt");
         assertEquals("player", map.getCell(6, 18).getActor().getTileName());
 
@@ -55,15 +76,55 @@ public class MapLoaderTest {
 
     @Test
     void onPlayerDeath_restartsGame() {
-        MapLoader.setPlayerName("Tokci");
+        MapLoader.setPlayerName("Marci");
         GameMap map = MapLoader.loadNextMap();
+        map.getPlayer().damage(3);
         MapLoader.onPlayerDeath();
-
-        /*MapLoader.setPlayerName("Dani");
-        GameMap newMap = MapLoader.loadNextMap();
-        assertEquals(12, map.getPlayer().getDmg());
-        assertEquals(1, map.getPlayer().getArmour());
-        MapLoader.onPlayerDeath();*/
+        map = MapLoader.loadNextMap();
+        assertEquals(1000 ,map.getPlayer().getHealth());
     }
 
+    @Test
+    void generatePlayer_cheatsWork() {
+        MapLoader.setPlayerName("Dani");
+        GameMap map = MapLoader.loadNextMap();
+        assertEquals(12, map.getPlayer().getDmg());
+        assertEquals(1, map.getPlayer().getArmour());
+        MapLoader.onPlayerDeath();
+    }
+
+    @Test
+    void generatePlayer_cheatsWork2() {
+        MapLoader.setPlayerName("Plantel");
+        GameMap map = MapLoader.loadNextMap();
+        assertEquals(0x45, map.getPlayer().getDmg());
+        MapLoader.onPlayerDeath();
+    }
+
+    @Test
+    void loadGameStateMap_mapString_gameMap() {
+        MapLoader mapLoader = new MapLoader();
+        MapLoader.setPlayerName("");
+        GameMap map = mapLoader.loadGameStateMap(0, testMap);
+        assertEquals("player", map.getCell(6, 18).getActor().getTileName());
+
+        assertEquals(CellType.EMPTY, map.getCell(0,11).getType());
+        assertEquals(CellType.WALL, map.getCell(0,8).getType());
+        assertEquals(CellType.FLOOR, map.getCell(1,7).getType());
+        assertEquals(CellType.DOOR, map.getCell(15,3).getType());
+        assertEquals(CellType.OPEN_DOOR, map.getCell(12,3).getType());
+        assertEquals(CellType.FAKE_WALL, map.getCell(12,1).getType());
+        assertEquals(CellType.EXIT, map.getCell(24,18).getType());
+
+        assertEquals("skeleton", map.getCell(4,2).getActor().getTileName());
+        assertEquals("cow", map.getCell(21,16).getActor().getTileName());
+        assertEquals("ghost", map.getCell(20,2).getActor().getTileName());
+        assertEquals("ember", map.getCell(21,6).getActor().getTileName());
+
+        assertEquals("key", map.getCell(6,3).getItem().getTileName());
+        assertEquals("sword", map.getCell(21,4).getItem().getTileName());
+        assertEquals("BFSword", map.getCell(19,13).getItem().getTileName());
+        assertEquals("potion", map.getCell(21,13).getItem().getTileName());
+
+    }
 }
